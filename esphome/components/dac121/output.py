30bc9d0615f85f0ae58b068bfb42edb0bff90e8d
Padm59 <philadm59@gmail.com>
2024-09-05 13:32:50 +0200

    Output einstellen

--- esphome/components/dac121/output.py
+++ esphome/components/dac121/output.py
@@ -1,27 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import output
+from esphome.components import i2c
 from esphome.const import CONF_CHANNEL, CONF_ID
-from . import DAC7678Output, dac7678_ns
+from . import DAC121Output
 
-DEPENDENCIES = ["dac7678"]
 
-DAC7678Channel = dac7678_ns.class_("DAC7678Channel", output.FloatOutput)
-CONF_DAC7678_ID = "dac7678_id"
+
+DEPENDENCIES = ["i2c"]
+MULTI_CONF = True
+
+
+dac121_ns = cg.esphome_ns.namespace("dac121")
+DAC121 = dac121_ns.class_("DAC121", output.FloatOutput, cg.Component)
+CONF_DAC121_ID = "dac121_id"
 
 CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
     {
-        cv.Required(CONF_ID): cv.declare_id(DAC7678Channel),
-        cv.GenerateID(CONF_DAC7678_ID): cv.use_id(DAC7678Output),
-        cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=7),
+        cv.GenerateID(CONF_DAC121_ID): cv.use_id(DAC121Output),
     }
-)
+    
+).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x09))
 
 
 async def to_code(config):
-    paren = await cg.get_variable(config[CONF_DAC7678_ID])
     var = cg.new_Pvariable(config[CONF_ID])
-    cg.add(var.set_channel(config[CONF_CHANNEL]))
-    cg.add(paren.register_channel(var))
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
     await output.register_output(var, config)
     return var
