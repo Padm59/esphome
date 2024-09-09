5230ca9285f8bc026ab52f1f113d2025b8d084db
Padm59 <philadm59@gmail.com>
2024-09-09 09:54:38 +0200

    more log

--- esphome/components/dac121/dac121_output.cpp
+++ esphome/components/dac121/dac121_output.cpp
@@ -51,7 +51,7 @@ void DAC121::write_state(float state) {
   uint8_t send[2];
   this->uint16_to_array(value, send);
   if(!this->write(send, 2)) {
-    ESP_LOGV(TAG, "Data send %f",state);
+    ESP_LOGV(TAG, "Data send: %f Output: %u", state, this->data_);
   } else {
     ESP_LOGE(TAG, "Failed to send new State");
   }
