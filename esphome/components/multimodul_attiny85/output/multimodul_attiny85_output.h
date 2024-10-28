5eb1647eda58687a631d4e3d143e6b06a648e030
Padm59 <philadm59@gmail.com>
2024-10-28 10:52:26 +0100

    fix9

--- esphome/components/multimodul_attiny85/output/multimodul_attiny85_output.h
+++ esphome/components/multimodul_attiny85/output/multimodul_attiny85_output.h
@@ -38,7 +38,7 @@ template<typename... Ts> class SetFrequencyAction : public Action<Ts...> {
   }
   
  protected:
-  MultiModulATtiny85Component *parent_;
+  MultiModulATtiny85Output *parent_;
 };
 
 }  // namespace multimodul_attiny85
